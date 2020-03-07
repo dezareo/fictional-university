5c9a70a05ec24ce4e58656cd7573fb46f15478ef
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2020-03-07 14:25:00 +0100

    Create WP loop

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
+
+	while(have_posts()) {
+		the_post(); ?>
+		
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<?php the_content(); ?>
+		<hr />
+		
+	<?php } 
 	
-	function greet($name) {
-		echo "<p>Hi, my name is $name ...</p>";
-	}
-	greet('Dragan');
 ?>
